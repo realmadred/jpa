dfc2c3fff82d8440c2c5f8bcea5670523d3ccf7a
liufeng <liufeng@hongte.info>
2018-04-27 17:32:39 +0800

    ueditor image

--- src/main/java/com/example/jpa/controller/ImageController.java
+++ src/main/java/com/example/jpa/controller/ImageController.java
@@ -1,23 +1,52 @@
 package com.example.jpa.controller;
 
-import com.example.jpa.common.Constant;
+import com.baidu.ueditor.ActionEnter;
+import com.example.jpa.config.ImageConfig;
 import org.apache.commons.lang3.RandomStringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.File;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 import java.util.*;
 
 @Controller
-@RequestMapping("/img")
+@RequestMapping("/ueditor")
 public class ImageController {
 
-    @RequestMapping(value = "/update")
+    @Autowired
+    private ImageConfig imageConfig;
+
+    @GetMapping("/base")
+    public void base(HttpServletRequest request, HttpServletResponse response) throws UnsupportedEncodingException {
+        request.setCharacterEncoding( "utf-8" );
+        response.setHeader("Content-Type" , "text/html");
+        String path = imageConfig.getPath();
+        String configPath = ImageController.class.getResource("/").getPath();
+        try {
+            String action = request.getParameter("action");
+            if("config".equals(action)){
+                path = configPath;
+            }
+            PrintWriter writer = response.getWriter();
+            String exec = new ActionEnter(request, path, configPath + "config.json").exec();
+            writer.write(exec.replaceAll(path,imageConfig.getBaseUrl()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @RequestMapping(value = "/upload")
     @ResponseBody
     public String imgUpdate(MultipartHttpServletRequest request) {
         MultiValueMap<String, MultipartFile> multiFileMap = request.getMultiFileMap();
@@ -34,11 +63,11 @@ public class ImageController {
                 // 获取文件的后缀名
                 String suffixName = fileName.substring(fileName.lastIndexOf("."));
                 // 这里我使用随机字符串来重新命名图片
-                fileName = Calendar.getInstance().getTimeInMillis() + RandomStringUtils.randomAlphabetic(4) + suffixName;
+                fileName = System.currentTimeMillis() + RandomStringUtils.randomAlphabetic(4) + suffixName;
                 // 这里的路径为Nginx的代理路径，这里是/data/images/xxx.png
-                String realPath = ImageController.class.getResource("/").getPath();
-                String filePath = realPath + Constant.STATIC_PATH+Constant.IMAGE_UPDATE_PATH;
-                File dest = new File(filePath + fileName);
+//                String realPath = ImageController.class.getResource("/").getPath();
+//                String filePath = realPath + Constant.STATIC_PATH+Constant.IMAGE_UPDATE_PATH;
+                File dest = new File(imageConfig.getPath() + fileName);
                 // 检测是否存在目录
                 if (!dest.getParentFile().exists()) {
                     dest.getParentFile().mkdirs();
@@ -47,12 +76,10 @@ public class ImageController {
                     file.transferTo(dest);
                     //url的值为图片的实际访问地址 这里我用了Nginx代理，访问的路径是http://localhost/xxx.png
                     result = "{\"state\": \"SUCCESS\"," +
-                            "\"url\": \"" + Constant.BASE_URL + Constant.IMAGE_UPDATE_PATH + fileName + "\"," +
+                            "\"url\": \"" + imageConfig.getBaseUrl() + fileName + "\"," +
                             "\"title\": \"" + fileName + "\"," +
                             "\"original\": \"" + fileName + "\"}";
-                } catch (IllegalStateException e) {
-                    e.printStackTrace();
-                } catch (IOException e) {
+                } catch (IllegalStateException | IOException e) {
                     e.printStackTrace();
                 }
             }
