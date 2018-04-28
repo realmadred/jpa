d26a1e9bc4fd61d2347f076a2da6962264d0f790
liufeng <liufeng@hongte.info>
2018-04-28 17:48:23 +0800

    js

--- src/test/java/com/example/jpa/js/JsTest.java
+++ src/test/java/com/example/jpa/js/JsTest.java
@@ -3,10 +3,8 @@ package com.example.jpa.js;
 import org.apache.commons.lang3.time.StopWatch;
 import org.junit.Test;
 
-import javax.script.Invocable;
-import javax.script.ScriptEngine;
-import javax.script.ScriptEngineManager;
-import javax.script.ScriptException;
+import javax.script.*;
+import java.util.concurrent.TimeUnit;
 
 public class JsTest {
 
@@ -28,6 +26,11 @@ public class JsTest {
             "    //print('Hi there from Javascript, ' + name);\n" +
             "    return \"greetings from javascript\";\n" +
             "};";
+    String js2 = "var fun1 = function(name) {\n" +
+            "    print('Hi there from Javascript js2, ' + name);\n" +
+            "    return \"greetings from javascript\";\n" +
+            "};";
+
 
     @Test
     public void test() {
@@ -84,11 +87,12 @@ public class JsTest {
             scriptEngine.eval(s1);
             scriptEngine.eval(script);
             scriptEngine.eval(js);
+            scriptEngine.eval(js2);
             StopWatch started = StopWatch.createStarted();
-            for (int i = 0; i <1000000 ; i++) {
+            for (int i = 0; i <10000 ; i++) {
                 Invocable invocable = (Invocable) scriptEngine;
-                invocable.invokeFunction("calculate", 56800000000000023L,13.9);
-                invocable.invokeFunction("printSalary", 1200,"name");
+//                invocable.invokeFunction("calculate", 56800000000000023L,13.9);
+//                invocable.invokeFunction("printSalary", 1200,"name");
                 invocable.invokeFunction("fun1", "abcd");
             }
             started.stop();
@@ -97,4 +101,98 @@ public class JsTest {
             e.printStackTrace();
         }
     }
+
+    @Test
+    public void test5() throws ScriptException, NoSuchMethodException {
+        ScriptEngineManager manager = new ScriptEngineManager();
+        ScriptEngine engine = manager.getEngineByName("JavaScript");
+
+        // 用String定义了一段JavaScript代码这段代码定义了一个对象'obj'
+        // 给对象增加了一个名为 hello 的方法（hello这个方法是属于对象的）
+        String script = "var obj = new Object(); obj.hello = function(name) { print('Hello, ' + name); }";
+        //执行这段script脚本
+        engine.eval(script);
+
+        // javax.script.Invocable 是一个可选的接口
+        // 检查你的script engine 接口是否已实现!
+        // 注意：JavaScript engine实现了Invocable接口
+        Invocable inv = (Invocable) engine;
+
+        // 获取我们想调用那个方法所属的js对象
+        Object obj = engine.get("obj");
+
+        // 执行obj对象的名为hello的方法
+        inv.invokeMethod(obj, "hello", "Script Method !!" );
+    }
+
+    @Test
+    public void test6() throws ScriptException, InterruptedException {
+        ScriptEngineManager manager = new ScriptEngineManager();
+        ScriptEngine engine = manager.getEngineByName("JavaScript");
+
+        // String里定义一段JavaScript代码脚本
+        String script = "function run() { print('run called'); }";
+
+        // 执行这个脚本
+        engine.eval(script);
+
+        Invocable inv = (Invocable) engine;
+
+        // 从脚本引擎中获取Runnable接口对象（实例）. 该接口方法由具有相匹配名称的脚本函数实现。
+// 在上面的脚本中，我们已经实现了Runnable接口的run()方法
+        Runnable r = inv.getInterface(Runnable.class);
+
+        // 启动一个线程运行上面的实现了runnable接口的script脚本
+        Thread th = new Thread(r);
+        th.start();//打印输出：run called
+        TimeUnit.SECONDS.sleep(1);
+    }
+
+    @Test
+    public void test7() throws ScriptException {
+        ScriptEngineManager manager = new ScriptEngineManager();
+        ScriptEngine engine = manager.getEngineByName("JavaScript");
+
+        // String里定义一段JavaScript代码脚本
+        String script = "var obj = new Object(); obj.run = function() { println('run method called'); }";
+
+        // 执行这个脚本
+        engine.eval(script);
+
+        // 获取这个我们要用来实现接口的js对象
+        Object obj = engine.get("obj");
+
+        Invocable inv = (Invocable) engine;
+
+        // 通过脚本引擎获取Runnable接口对象 该接口的方法已被js对象实现
+        Runnable r = inv.getInterface(obj, Runnable.class);
+
+        // 启动一个线程，运行已被脚本实现的Runnable接口
+        Thread th = new Thread(r);
+        th.start();
+    }
+
+    @Test
+    public void testScop() throws ScriptException {
+        ScriptEngineManager manager = new ScriptEngineManager();
+        ScriptEngine engine = manager.getEngineByName("JavaScript");
+
+        engine.put("x", "hello");
+        // 打印全局变量 "x"
+        engine.eval("print(x);");
+        // 上面的代码会打印"hello"
+
+        // 现在，传入另一个不同的script context
+        ScriptContext newContext = new SimpleScriptContext();
+//新的Script context绑定ScriptContext的ScriptContext
+        Bindings engineScope = newContext.getBindings(ScriptContext.ENGINE_SCOPE);
+
+        // 增加一个新变脸到新的范围 engineScope 中
+        engineScope.put("x", "world");
+
+        // 执行同一个脚本 - 但这次传入一个不同的script context
+        engine.eval("print(x);", newContext);
+        // 上面的代码会打印 "world"
+    }
+
 }
